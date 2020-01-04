5a02bc0f5a4f8bf44490222c235038a009d81411
Satvir Sandhu <356Sandhu@gmail.com>
2020-01-03 19:12:24 -0500

    generate primary and secondary color

---
+++ app.js
@@ -0,0 +1,16 @@
+const box1 = document.getElementById('box-1');
+const box2 = document.getElementById('box-2');
+
+generateRandomH = () =>{
+  const x = Math.round(Math.random() * 360);
+  return x;
+};
+
+primaryColor = generateRandomH();
+secondaryColor = primaryColor - 180;
+if(secondaryColor < 0){
+  secondaryColor = 360 + secondaryColor;
+}
+
+box1.style.backgroundColor = `hsl(${primaryColor}, 50%, 50%)`;
+box2.style.backgroundColor = `hsl(${secondaryColor}, 50%, 50%)`;
