9d806a80c1c2adaa2867b4266522903964048110
Satvir Sandhu <356Sandhu@gmail.com>
2020-01-04 14:04:16 -0500

    add anagulous colors

--- app.js
+++ app.js
@@ -1,18 +1,62 @@
-const box1 = document.getElementById('c-1');
-const box2 = document.getElementById('c-2');
+const boxc1 = document.getElementById('c-1');
+const boxc2 = document.getElementById('c-2');
+const boxa1 = document.getElementById('a-1');
+const boxa2 = document.getElementById('a-2');
+const boxa3 = document.getElementById('a-3');
+const cRefresh = document.getElementById('cRefresh');
+const aRefresh = document.getElementById('aRefresh');
 
-generateRandomH = () =>{
+const generateRandomHue = () =>{
   const x = Math.round(Math.random() * 360);
   return x;
 };
 
-primaryColor = generateRandomH();
-secondaryColor = primaryColor - 180;
-if(secondaryColor < 0){
-  secondaryColor = 360 + secondaryColor;
+const enforceColorWheel = (hue) =>{
+  if(hue < 0){hue = 360 + hue;}
+  return hue;
+};
+
+const generateString = (hue) => `hsl(${hue}, 60%, 50%)`;
+
+const generateComplimentaryHue = pHue => {
+  let sHue = enforceColorWheel(pHue - 180);
+  return sHue;
+};
+
+const generateComplimentaryPair = () =>{
+  const pHue = generateRandomHue();
+  const sHue = generateComplimentaryHue(pHue);
+  const pColor = generateString(pHue);
+  const sColor = generateString(sHue);
+  return [pColor, sColor];
+};
+
+const generateAnagulousPair = () =>{
+  const pHue = generateRandomHue();
+  const sHue = enforceColorWheel(pHue + 30);
+  const tHue = enforceColorWheel(pHue - 30);
+  const pColor = generateString(pHue);
+  const sColor = generateString(sHue);
+  const tColor = generateString(tHue);
+  return [pColor, sColor, tColor];
 }
 
-box1.style.backgroundColor = `hsl(${primaryColor}, 50%, 50%)`;
-box2.style.backgroundColor = `hsl(${secondaryColor}, 50%, 50%)`;
-box1.style.color = `hsl(${secondaryColor}, 50%, 50%)`;
-box2.style.color = `hsl(${primaryColor}, 50%, 50%)`;
+const paintComplimentary = () => {
+  const colors = generateComplimentaryPair();
+  boxc1.style.backgroundColor = colors[0];
+  boxc2.style.backgroundColor = colors[1];
+};
+
+const paintAnagulous = () => {
+  const colors = generateAnagulousPair();
+  boxa1.style.backgroundColor = colors[0];
+  boxa2.style.backgroundColor = colors[1];
+  boxa3.style.backgroundColor = colors[2];
+};
+
+cRefresh.addEventListener('click', paintComplimentary);
+aRefresh.addEventListener('click', paintAnagulous);
+
+paintComplimentary();
+paintAnagulous();
+
